9a5357ffb1df67324596650f5630643b325ea29b
NoeOnDev <alxg5516@gmail.com>
2025-05-11 20:53:43 -0600

    refactor: elimina el controlador y las solicitudes de productos, y ajusta las rutas de la API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,12 +15,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => 'Recurso no encontrado',
-                    'error' => 'Not Found'
-                ], 404);
-            }
-        });
+        //
     })->create();
